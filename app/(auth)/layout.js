61a368148e2e4670abe6b2cd982206fcf31263dd
Jubayer Hasan <jubaihasan88@gmail.com>
2026-02-07 00:22:22 +0600

    implement multiple layout system

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,15 +1,15 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import "../globals.css";
 import Navbar from "@/components/Navbar";
 import { dbConnect } from "@/service/mongo";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -26,7 +26,7 @@ export default async function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar></Navbar>
+        <Navbar sideMenu={false}></Navbar>
         <main>
           {children}
         </main>
